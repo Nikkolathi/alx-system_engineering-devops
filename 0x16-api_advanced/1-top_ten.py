ee3a707ce9f505915846284c60d455d8c22914c0
Nikkolathi <mainginicholus@gmail.com>
2024-01-16 12:48:21 +0000

    advanced api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""prints the titles of the first 10 hot posts listed for a given subreddit.
-
 """
-import requests
+queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit
+"""
+from requests import get
 
 
 def top_ten(subreddit):
-    '''Return the title of the first 10 hot posts
-    listed for a given subreddit'''
+    """prints titles of first 10 hot posts listed for a given subreddit"""
+    r_subreddit = get('http://www.reddit.com/r/{}/hot.json'.
+                      format(subreddit), headers={'User-Agent': 'Mybot'})
     try:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
-            (by /u/Large_Alternative_30)",
-        }
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if (response.status_code == 404):
-            print('None')
-            return 0
-        request = response.json().get('data').get('children')
+        subreddit_dict = r_subreddit.json()
         for i in range(10):
-            print(request[i].get('data').get('title'))
-    except Exception:
-        print('None')
-        return 0
+            print(subreddit_dict['data']['children'][i]['data']['title'])
+    except:
+        print(None)
